ed6770c120b7e90fe99a4b588cdef4a3c30caf9d
wtfarooq <b00080205@aus.edu>
2021-06-05 04:37:29 +0400

    separate manga page, prev-next chapters

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const poketo = require('poketo')
+const moment = require('moment');
 
 const app = express()
 const port = 3000
@@ -13,35 +14,66 @@ app.use('/public', express.static(__dirname + '/static'));
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-    console.log("request for home")
+    console.log("get request for home")
     res.render('index.njk')
 }, error=> {
     console.log(error.message)
 })
 
 app.post('/', (req, res) => {
-    console.log(req.body)
+    console.log('post request for : ' + req.body.manga)
     let manga = req.body.manga
-    let chapter = req.body.chapter
     let manganame = manga.split(' ').join('_')
-    let pad = "000"
-    let chapternum = pad.substring(0, pad.length - chapter.length) + chapter
-    res.redirect('/'+manganame+'/'+chapternum)
+    res.redirect('/'+manganame)
+})
+
+app.get('/:manganame', (req, res) => {
+    let output = []
+    let seriesurl = poketo.constructUrl('manga-fox:' + req.params.manganame)
+    console.log('get request for : ' + seriesurl)
+
+    poketo.getSeries(seriesurl).then(series => {
+        series.chapters.forEach((o) => {
+            output.push({ number: o.chapterNumber? o.chapterNumber : 0, title: o.title? o.title.split('&apos;').join('\''): '', time : moment.duration(Date.now()/1000 - o.createdAt, 'seconds').humanize() });
+        })
+        console.log(output)
+        res.render('manga.njk', {output : output, manga : req.params.manganame.split('_').join(' '), description :series.description, cover : series.coverImageUrl})
+    }, error => {
+        res.render('error.njk', { message : 'that manga series does not exist, try using the japanese name of manga' })
+    })
+}, error=> {
+    console.log(error.message)
 })
 
 app.get('/:manganame/:chapternum', (req, res) => {
     let output = []
-    let chapterurl = poketo.constructUrl('manga-fox:' + req.params.manganame + ':c' + req.params.chapternum)
-    console.log('request for : ' + chapterurl)
+    let pad = "000"
+    let chap = pad.substring(0, pad.length - req.params.chapternum.length) + req.params.chapternum
+    let chapterurl = poketo.constructUrl('manga-fox:' + req.params.manganame + ':c' + chap)
+    console.log('get request for : ' + chapterurl)
+
+    let serieslist = []
+    let prev
+    let next
+    let seriesurl = poketo.constructUrl('manga-fox:' + req.params.manganame)
+    poketo.getSeries(seriesurl).then(series =>{
+        series.chapters.forEach((o) => {
+            serieslist.push(o.chapterNumber? o.chapterNumber : 0)
+        })
+        let index = serieslist.lastIndexOf(parseFloat(req.params.chapternum).toString())
+        prev = serieslist[index+1] ? serieslist[index+1]: 'no'
+        next = serieslist[index-1] ? serieslist[index-1]: 'no'
+        // console.log(req.params.chapternum, serieslist, index ,prev, next)
+    })
 
     poketo.getChapter(chapterurl).then(chapter => {
         chapter.pages.forEach((o) => {
             output.push(o.url)
         })
         console.log(output)
-        res.render('page.njk', {output : output, manga : req.params.manganame, chapter : req.params.chapternum})
+        res.render('page.njk', {output : output, manga : req.params.manganame, chapter : req.params.chapternum, prev: prev, next: next})
     }, error => {
-        res.render('error.njk', { message : 'no such manga chapter exists, try using the japanese name of manga' })
+        res.render('error.njk', { message : 'that manga chapter does not exist' })
     })
 }, error=> {
     console.log(error.message)
